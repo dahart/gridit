c5e501ba1fadb9fcd0902ea6c8c3e528468583cd
David Hart <dahart@gmail.com>
2013-11-02 22:27:15 -0600

    sync changes from lineup to alignerator

--- alignerator.py
+++ alignerator.py
@@ -17,7 +17,6 @@ except ImportError:
     del sys.path[-1]
     normed_rowcol = indentation.line_and_normed_pt
 
-
 #----------------------------------------------------------------------
 
 # riffle, as in riffle (perfect shuffle) a deck of cards
@@ -30,25 +29,76 @@ def riffle(*args):
 # return the print format string for right-justify if we have a number
 # or left-justify if we have anything else
 def trJustify(x):
-    if x == 'num': return '>'
+    if x == 'N' or x == 'O': return '>'
     return '<'
 
 # collapse the text of whitespace pairs to a single space
 def trSpace(x):
-    if x[0] == 'ws' and len(x[1]) > 0: return (x[0],' ')
+    if x[0] == 'S' and len(x[1]) > 0: return (x[0],' ')
     return x
 
 # expand separable chars with whitespace on either side
 def trSep(x):
-    if x[0] == 'sep': return [('ws', ''), x, ('ws', '')];
+    if x[0] == 'C': return [('S', ''), x, ('S', '')];
     return [x];
 
 # replace consecutive ws's with a single one
 def reduceWS(l, x):
-    if x[0] == 'ws' and len(l) > 0 and l[-1][0] == 'ws':
-        return l[:-1] + [('ws', ' ' * max(len(x[1]), len(l[-1][1])))]
+    if x[0] == 'S' and len(l) > 0 and l[-1][0] == 'S':
+        return l[:-1] + [('S', ' ' * max(len(x[1]), len(l[-1][1])))]
     else:
         return l + [x]
+
+# http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Levenshtein_distance#C.2B.2B
+def edist(s1, s2):
+    d_curr = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        d_prev, d_curr = d_curr, [i]
+        for j, c2 in enumerate(s2):
+            d_curr.append(min(
+              d_prev[j] + (c1 != c2),
+              d_prev[j + 1] + 1,
+              d_curr[j] + 1
+            ))
+    return d_curr[len(s2)]
+
+
+# http://en.wikipedia.org/wiki/Levenshtein_distance
+def btedist(s, t, distfunc):
+    (m,n) = len(s), len(t)
+    d = [[0 for x in range(0, n+1)] for x in range(0, m+1)]
+    b = [[0 for x in range(0, n+1)] for x in range(0, m+1)]
+    for i in range(0, m+1): d[i][0] = i
+    d[0] = range(0, n+1)
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            (b[i][j], d[i][j]) = min(
+                (0, d[i-1][j]+1),
+                (1, d[i][j-1]+1),
+                (2, d[i-1][j-1] + distfunc(s[i-1], t[j-1])),
+                key=lambda x:x[1])
+    (i, j) = (m, n)
+    bt = [];
+    while (i >= 1 and j >= 1):
+        bt.append(b[i][j])
+        if (b[i][j] == 0): i -= 1
+        elif (b[i][j] == 1): j -= 1
+        elif (b[i][j] == 2): i -= 1; j -= 1
+    bt.reverse()
+    return (d[m][n], bt)
+
+
+def matchTypeDist(m1, m2):
+    if m1[0] != m2[0]: return 1.0
+    if m1[0] == 'S': return 0.0
+    score = 0.0
+    if m1[1] == '':
+        score = len(m2[1])
+    elif m2[1] == '':
+        score = len(m1[1])
+    else:
+        score = edist(m1[1], m2[1])
+    return score / (1.0 + score)
 
 #----------------------------------------------------------------------
 
@@ -57,116 +107,104 @@ class AligneratorCommand(sublime_plugin.TextCommand):
         super(AligneratorCommand, self).__init__(*args, **kwargs)
 
         # capture 'words'
-        separableChars = "\[\]\{\}\(\)\;\,"
+        separableChars = r"""\[\]\{\}\(\)\;\,"""
+        quoteChars = r"""\'\""""
 
-        matcher = r"""
-        (?P<ws>\s+)                                 # white space
-        |(?P<quote>(?P<q>[\'\"]).*?(?P=q))          # quoted items
-        |(?P<num>[+-]?(?<=\b)((\d+(\.\d*)?)|\.\d+)([eE][+-]?[0-9]+|f)?(?=\b)) # match ints & floats - dunno if the word boundary look-around matches are still needed...
-        |(?P<sep>[""" + separableChars + """])      # any separable char
-        |(?P<word>[^\s""" + separableChars + """]+) # groups of non-separable, non-whitespace chars
+        # |(?P<O>[-\+/\*%&\|\^]=|=|&&|\|\||<<=?|>>=?|::)       # 'O'perators- mainly this is any non-breakable groups of otherwise separable chars
+
+        matcher = r"""(?P<S>\s+)                               # 'S'pace, of the white variety
+        |(?P<Q>(?P<qc>\'+|\"+)([^\\]|\\.)*?(?P=qc))            # 'Q'uoted items
+        |(?P<N>[-+]?(\d+(\.\d*)?|\.\d+|0[xX][\dA-Fa-f]+)([eE][-+]?\d+)?) # 'N'ums: match ints & floats
+        |(?P<O>[-\+/\*%&\|\^]=|\[\]|\{\}|\(\))                 # 'O'perators- mainly this is any non-breakable groups of otherwise separable chars
+        |(?P<C>[""" + separableChars + r"""])                  # 'C'har: anything separable
+        |(?P<W>[^\s""" + quoteChars + separableChars + r"""]+) # 'W'ord: groups of non-separable, non-whitespace chars
         """
 
         self.e = re.compile(matcher, re.VERBOSE)
 
+
     def run(self, edit):
         # read input lines
-
         view = self.view
         sel = view.sel()
-
         if len(sel) != 1:
             print('Alignerator: try selecting one (and only one) region')
             return
-
-        # lineRegs = []
-        # for reg in sel:
-        #     for line in view.lines(reg):
-        #         lineRegs.append(line)
-
         oldText = view.substr(sel[0])
-
-        # lines = [ view.substr(reg) for reg in lineRegs ]
-
         lines = oldText.split('\n')
 
-        # print("lines:\n", lines)
-        # print('Alignerating')
-
-        lists = []
-        justifies = []
+        allmatches = []
         for line in lines:
             # snag all matches
             lineiter = self.e.finditer(line)
             # expand all matches by name
-            groups = [match.groupdict() for match in lineiter]
+            groups = [ match.groupdict() for match in lineiter ]
+            print(groups)
             # groups includes all non-matches- filter those out
             matches = [ [x for x in g.items() if x[1] != None][0] for g in groups ]
-            # print(matches)
+            if not matches:
+                allmatches.append(matches)
+                continue
+            # add null ws between separable chars
+            matches = [ m for mg in matches for m in trSep(mg) ]
+            # now delete multiple whitespaces in a row
+            matches = functools.reduce(reduceWS, [[matches[0]]] + matches[1:])
+            print(matches)
+            allmatches.append(matches)
 
-            if len(matches) == 0:
-                lists.append([''])
+        # we'll align all lines to the longest matches list
+        template = max( allmatches, key=lambda m:len(m) )
+
+        chunks, justifies = [], []
+        for matches in allmatches:
+            if not matches:
+                chunks.append([''])
                 justifies.append([''])
-            else:
-                # add null ws between separable chars
-                matches = functools.reduce(operator.concat, [ trSep(m) for m in matches ]);
-                # now delete multiple whitespaces in a row - only propagate the largest WS
-                # in order for reduce to return a list of tuples, I have to listify
-                # the first element of the list of tuples, so that concatenate is operating
-                # on a list, and not on the first tuple.  get it?!?!
-                matches = functools.reduce(reduceWS, [[matches[0]]] + matches[1:]);
-                # collapse all non-zero whitespace matches to single spaces
-                (matchNames, tokenList) = zip(*[ trSpace(m) for m in matches])
-
-                # print('matchNames:',matchNames)
-                # print('list:',tokenList)
-
-                # except restore the very first column if it was whitespace
-                # we don't want the overall indentation to change
-                if matchNames[0] == 'ws':
-                    tokenList = (matches[0][1],) + tokenList[1:]
-                justify = [ trJustify(name) for name in matchNames ]
-
-                # remember our text columns & justification
-                lists.append(tokenList)
-                justifies.append(justify)
+                continue
+            # align matches & template, stuff whitespace into all missing slots
+            (d, bt) = btedist(matches, template, matchTypeDist)
+            for i in range(len(bt)):
+                if bt[i] == 1:
+                    matches = matches[:i] + [('S','')] + matches[i:]
+            # collapse all non-zero whitespace matches to single spaces
+            (matchNames, chunk) = zip(*[ trSpace(m) for m in matches])
+            # except restore the very first column if it was whitespace
+            # we don't want the overall indentation to change
+            if matchNames[0] == 'S':
+                chunk = (matches[0][1],) + chunk[1:]
+            justify = [ trJustify(name) for name in matchNames ]
+            # remember our text columns & justification
+            chunks.append(chunk)
+            justifies.append(justify)
 
         # compute column widths
-        maxCols = max([len(l) for l in lists])
+        maxCols = max([len(l) for l in chunks])
         widths = [0 for x in range(maxCols)]
         # justify = ['-' for x in widths]
-        for tokenList in lists:
+        for chunk in chunks:
             # zero-pad the end of the result, so that widths doesn't get truncated
             # because zip stops when the first list is empty
-            newWidths = [ len(t) for t in tokenList ] + [0] * (maxCols-len(tokenList))
+            newWidths = [ len(t) for t in chunk ] + [0] * (maxCols-len(chunk))
             widths = [ max(w) for w in zip(widths, newWidths) ]
 
         # now go back through all our lines and output with new formatting
         newLines = []
-        for (tokenList,justify) in zip(lists,justifies):
-            n = len(tokenList)
-
+        for (chunk, justify) in zip(chunks, justifies):
+            n = len(chunk)
             a = ['{:'] * n
             b = justify
             c = [ str(w) if w > 0 else '' for w in widths ]
             d = ['s'] * n
             e = ['}'] * n
-
             fmt = ''.join(riffle(a,b,c,d,e))
-            # fmt = ''.join(a)
+            newLines.append(fmt.format(*chunk))
 
-            # print ('fmt:', len(fmt), fmt)
-            # print ('tok:', len(tokenList), tokenList)
-            # print (fmt.format(*tokenList))
-
-            newLines.append(fmt.format(*tokenList))
-
-        # for reg, newLine in zip(lineRegs, newLines):
         view.replace(edit, sel[0], '\n'.join(newLines))
 
-# 1 2 3 + 0 = 0 ;
-# 11 22 33 + 5 = 33333;
-# 111 222 333 + 555 = 3;
+                # 1 2 3 + 0 = 0 ;
+                # 11 22 33 + 5 = 33333;
+                # 111 222 333 + 555 = 3;
+                # 111 222 333 + 555 + 6667 = 3;
 
 
 def convert_to_mid_line_tabs(view, edit, tab_size, pt, length):
